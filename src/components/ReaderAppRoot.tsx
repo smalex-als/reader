88ef0ed6b3b8de51f73e92d8b179b202177927cb
Alexey Smychagin <smalex69@gmail.com>
2026-06-12 10:46:40 -0400

    inline reader runtime hooks

--- src/components/ReaderAppRoot.tsx
+++ src/components/ReaderAppRoot.tsx
@@ -1,14 +1,24 @@
 import ReaderMainContent from '@/components/ReaderMainContent';
 import ReaderModalLayer from '@/components/ReaderModalLayer';
 import ReaderSidebar from '@/components/ReaderSidebar';
+import { useBookSession } from '@/hooks/useBookSession';
+import { useDashboardNavigation } from '@/hooks/useDashboardNavigation';
+import { useHotkeys } from '@/hooks/useHotkeys';
+import { useOcrEditMode } from '@/hooks/useOcrEditMode';
 import { useReaderAudioControls } from '@/hooks/useReaderAudioControls';
-import { useReaderFeatureRuntime } from '@/hooks/useReaderFeatureRuntime';
 import { useReaderShellControls } from '@/hooks/useReaderShellControls';
 import { ReaderShellProvider, useReaderShell } from '@/hooks/useReaderShellContext';
+import { useShareLink } from '@/hooks/useShareLink';
+import { useUnitsRouteSync } from '@/hooks/useUnitsRoute';
 
 export default function ReaderAppRoot() {
   const shellControls = useReaderShellControls();
-  useReaderFeatureRuntime();
+  useBookSession();
+  useUnitsRouteSync();
+  useOcrEditMode();
+  useDashboardNavigation();
+  useShareLink();
+  useHotkeys();
   useReaderAudioControls();
 
   return (
