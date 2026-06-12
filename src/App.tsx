020a355413f7f16236e16f76e8231c4d4b19909c
Alexey Smychagin <smalex69@gmail.com>
2026-06-12 09:55:28 -0400

    render reader root directly

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import ReaderAppRoot from '@/components/ReaderAppRoot';
-
-export default function App() {
-  return <ReaderAppRoot />;
-}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReaderAppRoot from '@/components/ReaderAppRoot';
 import { AppStateProvider } from './state/appState';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppStateProvider>
-      <App />
+      <ReaderAppRoot />
     </AppStateProvider>
   </React.StrictMode>
 );
